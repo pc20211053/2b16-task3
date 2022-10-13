803df978c822f639b1bd965296008e87881c0cbb
pc20211053 <115679802+pc20211053@users.noreply.github.com>
2022-10-13 11:16:08 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    while (true) {
-    	
-    }
-})
-basic.forever(function () {
-	
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
+    basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showNumber(0)
 })
