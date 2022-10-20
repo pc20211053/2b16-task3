0c869077d535360b3d94a520c2ecf0efbc10a436
pc20211053 <115679802+pc20211053@users.noreply.github.com>
2022-10-20 10:45:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,47 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(9)
-    basic.showNumber(8)
-    basic.showNumber(7)
-    basic.showNumber(6)
-    basic.showNumber(5)
-    basic.showNumber(4)
-    basic.showNumber(3)
-    basic.showNumber(2)
-    basic.showNumber(1)
-    basic.showNumber(0)
+    count += -10
+})
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
